7fbce80ce1ebc2f83f96736844d21486aa5f5f7e
zhangweide <zhangweide@baidu.com>
2016-12-21 06:31:32 +0800

    push bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,4 +7,20 @@ sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net --recv-key 0xB01FA
 
 sudo apt-get update
 
-sudo apt-get install vim,git,cuda,ros-indigo-desktop-full,screen,exuberant-ctags
+sudo apt-get install vim,git,ros-indigo-desktop-full,screen,exuberant-ctags
+
+#Install docker 
+sudo apt-get install apt-transport-https ca-certificates
+
+sudo apt-key adv \
+	               --keyserver hkp://ha.pool.sks-keyservers.net:80 \
+				   --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+
+echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" | sudo tee /etc/apt/sources.list.d/docker.list 
+
+sudo apt-get update
+
+sudo apt-get install linux-image-extra-$(uname -r) linux-image-extra-virtual
+
+sudo reboot
+
